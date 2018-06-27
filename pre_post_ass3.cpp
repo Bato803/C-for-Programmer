18de5855916d05f97b81f98ba8a4df1123bfc353
Bato803 <dengbotaoo@gmail.com>
2018-06-26 20:59:11 -0400

    File Added: ass2 - ass3

---
+++ pre_post_ass3.cpp
@@ -0,0 +1,23 @@
+/*Goal: Practice PreFix and PostFix
+**
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b = 0;
+    int post, pre = 0;
+    cout<<"Inital values: \t\t\tpost = "<<post<<" pre= "<<pre<<"\n";
+    post = a++;
+    pre = ++b;
+    cout<<"After one postfix and prefix: \tpost = "<<post<<" pre= "<<pre<<"\n";
+    cout<<"Now \ta="<<a<<"\tb="<<b<<endl;
+    post = a++;
+    pre = ++b;
+    cout<<"After two postfix and prefix: \tpost = "<<post<<" pre= "<<pre<<"\n";  
+    cout<<"Now \ta="<<a<<"\tb="<<b<<endl;
+    return 0;
+}
