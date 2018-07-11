dea7c0d1259efcff3f517a8c31ca20ff3bc77fec
Botao Deng <dengbotaoo@gmail.com>
2018-07-11 12:39:40 -0400

    Create add_element_ass13.cpp

---
+++ add_element_ass13.cpp
@@ -0,0 +1,44 @@
+// constructing vectors
+// constructing vectors
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+  void printVector(vector<int> vIn);
+  void assignFunction(vector<int> vInts, int in);
+  void pushBackFunction(vector<int> vInts, int in);
+  void emplaceFunction(vector<int> vInts, int loc, int in);
+  
+ void printVector(vector<int> vIn)
+ {//printing the contents of vIns
+  //TODO: Finish the function
+  vector<int>::iterator it;
+  for(it=vIn.begin(); it<vIn.end(); it++){
+      std::cout << *it << std::endl;
+  }
+ }
+ 
+  void assignFunction(vector<int> vInts, int in)
+  {
+      cout<<"\nassigning "<<in<<" and printing the vector\n";
+      vInts.assign(1, in);
+      //TODO: Finish the function
+      printVector(vInts);
+  }
+  void pushBackFunction(vector<int> vInts, int in)
+  {
+      cout<<"\npush back "<<in<<" and printing the vector\n";
+      //TODO: Finish the function
+      vInts.push_back(in);
+      printVector(vInts);
+  }
+  void emplaceFunction(vector<int> vInts,  int loc, int in)
+  {
+      vector<int>::iterator it;
+      cout<<"\nemplacing "<<in<<" and printing the vector\n";
+      it = vInts.begin()+loc;
+      vInts.emplace(it, in);
+      //TODO: Finish the function
+      printVector(vInts);
+  }
